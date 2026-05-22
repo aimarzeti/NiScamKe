60ada1cb28241cf29aefd102b311dffe8562287f
kaylanaura <kaylachemad@gmail.com>
2026-05-23 02:37:10 +0800

    Delete popup.js

--- popup.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("diagnosticBtn").addEventListener("click", () => {
-    alert("System Diagnostic: Layer 1 Active. Local Sandbox operational. Spring Boot proxy pipelines status: Stable.");
-});
